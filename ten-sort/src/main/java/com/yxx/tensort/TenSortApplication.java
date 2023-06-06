abee8750406366b4a666ae06586f56bba2aa0651
qwe-ui <yxxwangb@163.com>
2023-06-06 18:35:37 +0800

    开始

---
+++ ten-sort/src/main/java/com/yxx/tensort/TenSortApplication.java
@@ -0,0 +1,13 @@
+package com.yxx.tensort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenSortApplication.class, args);
+    }
+
+}
---
+++ ten-sort/src/test/java/com/yxx/tensort/TenSortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxx.tensort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenSortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
